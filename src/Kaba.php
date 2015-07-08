06b995833c2ac243fd0cd115854669bd4a98c9c4
jhoughtelin <josh@findsomehelp.com>
2015-07-08 12:48:25 -0600

    adding code coverage push to scrutinizer from travis

--- src/Kaba.php
+++ src/Kaba.php
@@ -4,11 +4,64 @@ namespace CodeJet;
 
 class Kaba
 {
+    private $api_url = 'https://www.kaba-ecode.com/KWWS_WS/KWWSService.asmx?wsdl';
+    private $username;
+    private $password;
+
     /**
      * Kaba Class Construct
      */
-    public function __construct()
+    public function __construct($username, $password)
     {
-        // constructor body
+        $this->setUsername($username);
+        $this->setPassword($password);
+    }
+
+    /**
+     * @return string
+     */
+    public function getApiUrl()
+    {
+        return $this->api_url;
+    }
+
+    /**
+     * @param string $api_url
+     */
+    public function setApiUrl($api_url)
+    {
+        $this->api_url = $api_url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param mixed $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
     }
 }
